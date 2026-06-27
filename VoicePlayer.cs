a738caa579f91661820272d6cb1eb5bcd648db5c
Liezel19-RR <liezelramela@icloud.com>
2026-06-27 18:14:19 +0200

    Add files via upload

---
+++ VoicePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace part3
+{
+    internal class VoicePlayer
+    {
+    }
+}
+
+namespace cyber_security_bottttt
+{
+    class VoicePlayer
+    {
+        public void PlayVoice()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("C:\\Users\\Student\\source\\repos\\cyber security bottttt\\cyber security bottttt\\WhatsApp-Audio-2026-05-28-at-14.57.17-Copy-_2_ (1).wav");
+
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Voice file not found.");
+            }
+        }
+
+    }
+}
